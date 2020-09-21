4ebfeb00cbd91462bcc6b07167643419739a9a15
Isfar Oshir <iao@Isfars-iMac.fios-router.home>
2020-09-20 22:12:42 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,134 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+var session = require('client-sessions')
+const flash = require('connect-flash');
+
+const app = express()
+
+app.use(express.static("public"))
+app.use('/views', express.static(__dirname + '/views'))
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(session({
+    cookieName: 'session',
+    // secret: 'random_string_goes_here',
+    secret: "hey",
+    duration: 30 * 60 * 1000,
+    activeDuration: 5 * 60 * 1000,
+  }));
+app.use(flash())
+
+app.set('view engine', 'ejs')
+
+const adminSchema = new mongoose.Schema({
+    email: String,
+    password: String
+})
+
+const postSchema = new mongoose.Schema({
+    author: String,
+    title: String,
+    date: String,
+    content: String
+})
+
+const Admin = new mongoose.model("Admin", adminSchema)
+const Post = new mongoose.model("Post", postSchema)
+
+const URI = "mongodb+srv://Irfaz:Iz012789@cluster0.tyfbo.mongodb.net/chem?retryWrites=true&w=majority"
+
+const connectDB = async() => {
+    await mongoose.connect(URI, {useUnifiedTopology: true, useNewUrlParser: true})
+    console.log("db connected!")
+}
+
+connectDB()
+
+app.get('/', function(req, res){
+    var arr = []
+    Post.find({}, function(err, postArr){
+        if (err){
+            console.log(err)
+        }
+        else{
+            arr = postArr
+            res.render('index', {arr: arr});
+        }
+    })
+    
+})
+
+app.get('/admin', function(req,res){
+    res.render('admin')
+})
+
+app.get('/mission-control', function(req, res){
+    if (req.session.user){
+        res.render('mission-control')
+    }
+    else{
+        res.redirect("/admin")
+    }
+})
+
+app.post('/admin', function(req, res){
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    Admin.findOne({email: email}, function(err, foundAdmin){
+        if (err){
+            console.log(err);
+        }
+        else{
+            if (foundAdmin){
+                console.log(foundAdmin);
+                if (foundAdmin.password === password)
+                {
+                    req.session.user = foundAdmin
+                    console.log(req.session.user.email);
+                    res.redirect("/mission-control")
+                }
+                else{
+                    req.flash('message', 'Password is incorrect')
+                    res.redirect('/admin')
+                }
+            }
+            else{
+                req.flash('message', "Doesn't look like a user with this email exists")
+                res.redirect('/admin')
+            }
+        }
+    })
+})
+
+app.post("/createPost", function(req,res){
+    const author = req.body.author
+    const title = req.body.title
+    const date = req.body.date
+    const content = req.body.content
+
+    const newPost = new Post({
+        author: author,
+        title: title,
+        date: date,
+        content: content
+    })
+
+    newPost.save(function(err){
+        if (err){
+            console.log(err);
+        }
+        else{
+            res.redirect("/")
+        }
+    })
+})
+
+
+app.listen(8000, function(){
+    console.log("server is on 8000");
+})
+
+
---
+++ public/index.js
@@ -0,0 +1,161 @@
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.querySelector("#header1");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  console.log(sticky);
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+function myFunction2(event) {
+  // Declare variables
+  var input = document.getElementById('search');
+  var list = document.querySelectorAll(".Post")
+  
+  var arr = []
+  var word = ""
+  
+  // Loop through all list items, and hide those who don't match the search query
+  for (var i = 0; i < input.value.length; i++){
+    
+    if (i == input.value.length-1) {
+      word += input.value[i].toLowerCase()
+      arr.push(word)
+      word = ""
+    }
+    else if (input.value[i] != " "){
+      word += input.value[i].toLowerCase()
+    }
+    else{
+      arr.push(word)
+      word = ""
+    }
+  }
+  console.log(arr);
+  var arr_post = []
+  var title = ""
+  var content = ""
+  var tup = []
+  word = ""
+  for (var i = 0; i < list.length; i++){
+    title = list[i].childNodes[1].innerHTML.toLowerCase()
+    content = list[i].childNodes[3].innerHTML.toLowerCase()
+    for (var t = 0; t < title.length; t++){
+      if (t == title.length-1){
+        word += title[t]
+        tup.push(word)
+        word = ""
+      }
+      else if (title[t] != " "){
+        word += title[t]
+      }
+      else{
+        tup.push(word)
+        word = ""
+      }
+    }
+    for (var t = 0; t < content.length; t++){
+      if (t == content.length-1){
+        word += content[t]
+        tup.push(word)
+        word = ""
+      }
+      else if (content[t] != " "){
+        word += content[t]
+      }
+      else{
+        tup.push(word)
+        word = ""
+      }
+        
+  }
+    arr_post.push([list[i], tup])
+    tup = []
+}
+  console.log(arr_post);
+  var show = []
+  for (var i = 0; i < arr_post.length; i++){
+    var collection = arr_post[i][1]
+    console.log(collection);
+      for (var j = 0; j < arr.length; j++){
+      if (collection.includes(arr[j])){
+        if (!show.includes(arr_post[0])){
+          show.push(arr_post[i][0])
+        }
+      }
+    }
+  }
+  console.log(show);
+
+  for (var i = 0; i < list.length; i++){
+    if (show.length == 0){
+      list[i].style.display = ""
+    }
+    else if (show.includes(list[i])){
+      list[i].style.display = ""
+    }
+    else{
+      list[i].style.display = "none"
+    }
+  }
+}
+
+
+  // console.log("-");
+  // console.log(arr_post[0][1][0].innerHTML);
+  // console.log("-");
+  
+  // for (var i = 0; i < arr_post.length; i++){
+  //   for (var j = 0; j < arr.length; j++){
+
+  //     if (arr_post[0][1][0].innerHTML.toLowerCase() == (arr[j])){
+  //       console.log("match");
+
+  //     }
+  //   }
+  // }
+
+
+
+// function uniKeyCode(event){
+//   var key = event.keyCode;
+//   if (key == 13){
+//     var input = document.getElementById('search');
+//     var list = document.querySelectorAll(".Post")
+    
+//     var arr = []
+//     var word = ""
+    
+//     // Loop through all list items, and hide those who don't match the search query
+//     for (var i = 0; i < input.value.length; i++){
+    
+//       console.log(key);
+//       if (i == input.value.length-1) {
+//         word += input.value[i]
+//         arr.push(word)
+//         word = ""
+//       }
+//       else if (input.value[i] != " "){
+//         word += input.value[i]
+//       }
+      
+//       else{
+//         arr.push(word)
+//         word = ""
+//       }
+//     }
+//     console.log(arr);
+//   }
+  
+// }
