cda3d34243e058e4d27ca605b149fe8f2c48e950
Isfar Oshir <iao@Isfars-iMac.fios-router.home>
2021-01-02 04:29:48 -0500

    launch v3.2

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const postSchema = new mongoose.Schema({
 const Admin = new mongoose.model("Admin", adminSchema)
 const Post = new mongoose.model("Post", postSchema)
 
-const URI = "mongodb+srv://isfarO:testing321@cluster0.tyfbo.mongodb.net/chem-testing?retryWrites=true&w=majority"
+const URI = process.env.API_KEY
 
 const connectDB = async() => {
     await mongoose.connect(URI, {useUnifiedTopology: true, useNewUrlParser: true})
@@ -294,7 +294,7 @@ app.post("/save", function(req, res){
 
 
 
-app.listen(3001, function(){
+app.listen(process.env.PORT, function(){
     console.log("server is on 8000");
 })
 
